952cb2687b8b118a71ee76710ff96b7922a9a978
SERHII <4sergeyvoropaev@gmail.com>
2023-06-15 22:11:24 +0200

    Fixed 2 bugs

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-let firstAskedNumber = prompt("Type FIRST operand");
+let firstAskedNumber = Number (prompt("Type FIRST operand"));
 
-let leftOperand = Number(firstAskedNumber);
 
-let secondAskedNumber = prompt("Type SECOND operand");
+let secondAskedNumber = Number(prompt("Type SECOND operand"));
 
-let rightOperand = Number(secondAskedNumber);
 
 let operator = prompt("Typer OPERATOR");
 
 
 switch (operator) {
     case "+":
-        let resultAdd = alert (`${leftOperand}+${rightOperand} =` + (leftOperand + rightOperand))
+        alert (`${firstAskedNumber}+${secondAskedNumber} =` + (firstAskedNumber + secondAskedNumber))
         break;
     case "-":
-        let resultMinus = alert (`${leftOperand}-${rightOperand} =` + (leftOperand - rightOperand))
+        alert (`${firstAskedNumber}-${secondAskedNumber} =` + (firstAskedNumber - secondAskedNumber))
         break;
     // …
     case "*":
-        let resultMulty = alert (`${leftOperand}*${rightOperand} =` + (leftOperand * rightOperand))
+        alert (`${firstAskedNumber}*${secondAskedNumber} =` + (firstAskedNumber * secondAskedNumber))
         break;
     case "/":
-        let resultDivide = alert (`${leftOperand}/${rightOperand} =` + (leftOperand / rightOperand))
+        alert (`${firstAskedNumber}/${secondAskedNumber} =` + (firstAskedNumber / secondAskedNumber))
         break;
 }
 
