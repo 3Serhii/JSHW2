3c1eb7462d080ee0ef26ec283f5f082d79ab44dd
SERHII <4sergeyvoropaev@gmail.com>
2023-06-15 22:15:26 +0200

    Fixed 2 bugs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let firstAskedNumber = Number (prompt("Type FIRST operand"));
+let firstAskedNumber = Number(prompt("Type FIRST operand"));
 
 
 let secondAskedNumber = Number(prompt("Type SECOND operand"));
@@ -9,17 +9,17 @@ let operator = prompt("Typer OPERATOR");
 
 switch (operator) {
     case "+":
-        alert (`${firstAskedNumber}+${secondAskedNumber} =` + (firstAskedNumber + secondAskedNumber))
+        alert(`${firstAskedNumber}+${secondAskedNumber} =` + (firstAskedNumber + secondAskedNumber))
         break;
     case "-":
-        alert (`${firstAskedNumber}-${secondAskedNumber} =` + (firstAskedNumber - secondAskedNumber))
+        alert(`${firstAskedNumber}-${secondAskedNumber} =` + (firstAskedNumber - secondAskedNumber))
         break;
     // …
     case "*":
-        alert (`${firstAskedNumber}*${secondAskedNumber} =` + (firstAskedNumber * secondAskedNumber))
+        alert(`${firstAskedNumber}*${secondAskedNumber} =` + (firstAskedNumber * secondAskedNumber))
         break;
     case "/":
-        alert (`${firstAskedNumber}/${secondAskedNumber} =` + (firstAskedNumber / secondAskedNumber))
+        alert(`${firstAskedNumber}/${secondAskedNumber} =` + (firstAskedNumber / secondAskedNumber))
         break;
 }
 
